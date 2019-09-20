3008edc5c7346a6f9d2ca5d0f27ad1a188367915
zachsirera <z.sirera@gmail.com>
2019-09-19 21:34:07 -0500

    changed chart dimension

--- analyze.py
+++ analyze.py
@@ -216,7 +216,7 @@ def top25_plot():
 
 
 	fig = plt.figure(num=1)
-	fig.set_figheight(10)
+	fig.set_figheight(6)
 	fig.set_figwidth(10)
 	pos = np.arange(len(x))
 	plt.bar(pos, y, align='center', tick_label=x)
